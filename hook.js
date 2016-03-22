36dee1cb4285fe65a2c61c561f968ce028fa5913
Patrick Trasborg <patrick.trasborg@gmail.com>
2016-03-21 23:24:16 -0400

    removed gith, using express instead

--- hook.js
+++ hook.js
@@ -4,10 +4,6 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var Handlebars = require('handlebars');
 var chalk = require('chalk');
-var util = require('util');
-
-// Listen on port 9001
-// var gith = require('gith').create(9001);
 
 var app = express();
 app.use(bodyParser.json());
@@ -15,8 +11,31 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+var execFile = require('child_process').execFile;
+var execOptions = {
+  maxBuffer: 1024 * 1024
+};
+
 app.post('/', function(req, res){
-  console.log(chalk.yellow('got an incoming request', util.inspect(req.body, {showHidden: false, depth: null})));
+  if(req.body.ref && checkMaster(req.body.ref)){
+    console.log(chalk.yellow('incoming webhook on master'));
+    var template = fs.readFileSync(__dirname + '/template.hbs');
+    var createTemplate = Handlebars.compile(template.toString());
+    var inputData = {
+      repositoryName: 'nickells/rehash-spotify-jukebox',
+      projectName: 'rehash-spotify-jukebox',
+      projectStartFilePath: 'rehash-spotify-jukebox/app.js'
+    };
+
+    fs.writeFileSync(__dirname + '/hook.sh', createTemplate(inputData));
+
+    // Exec a shell script
+    execFile(__dirname + '/hook.sh', execOptions, function(error, stdout, stderr) {
+      if(error) console.log(chalk.red('Error executing script'));
+      else console.log(chalk.green('exec complete'));
+    });
+  }
+
   res.sendStatus(200);
 });
 
@@ -26,36 +45,7 @@ app.listen(app.get('port'), function(err){
   else console.log(chalk.green('listening on port 9001'));
 });
 
-// Import execFile, to run our bash script
-// var execFile = require('child_process').execFile;
-
-// var execOptions = {
-// 	maxBuffer: 1024 * 1024
-// };
-
-
-
-// gith({
-//   repo: 'nickells/rehash-spotify-jukebox'
-// }).on( 'all', function( payload ) {
-//   if( payload.branch === 'master' )
-//   {
-//     console.log(chalk.yellow('incoming payload on master', payload));
-//   	var template = fs.readFileSync(__dirname + '/template.hbs');
-//   	var createTemplate = Handlebars.compile(template.toString());
-//   	var inputData = {
-//   		repositoryName: 'pat310/OlympicWebScraper',
-//   		projectName: 'OlympicWebScraper',
-//   		projectStartFilePath: 'OlympicWebScraper/server.js'
-//   	};
-
-//   	fs.writeFileSync(__dirname + '/hook.sh', createTemplate(inputData));
-
-//     // // Exec a shell script
-//     // execFile(__dirname + '/hook.sh', execOptions, function(error, stdout, stderr) {
-//     //   // Log success in some manner
-//     //   if(error) console.log(chalk.red('Error executing script'));
-//     //   else console.log(chalk.green('exec complete'));
-//     // });
-//   }
-// });
+function checkMaster(str){
+  var masterString = str.split('/').pop();
+  return masterString === 'master';
+}
